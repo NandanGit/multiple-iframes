6ac130b2a2da9098f84c11ad8fed75e3ac47ff66
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-08-24 15:59:14 +0530

    Added an instance
    Added a hardcoded instance and made some tweeks to the addInstance form

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const DOM = {
 	// DOM Elements
+	instances: document.getElementById('instances'),
 };
 
 const Utils = {
@@ -8,6 +9,16 @@ const Utils = {
 
 const UI = {
 	// UI methods
+	addInstance({ name, url }) {
+		const instanceHTML = `<div class="instance">
+				<h3 class="instance__name">${name}</h3>
+				<iframe
+					class="instance__iframe"
+					src="${url}"
+					frameborder="0"
+				></iframe>`;
+		DOM.instances.insertAdjacentHTML('beforeend', instanceHTML);
+	},
 };
 
 const Logic = {
