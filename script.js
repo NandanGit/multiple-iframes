62afd75324ca70133055f9a92b691ab19d4a8bb4
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-08-24 16:11:13 +0530

    Added toggle functionality to the form

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const DOM = {
 	// DOM Elements
 	instances: document.getElementById('instances'),
+	addInstanceForm: document.getElementById('add-instance-form'),
+	addInstanceButton: document.getElementById('add-instance'),
 };
 
 const Utils = {
@@ -19,8 +21,15 @@ const UI = {
 				></iframe>`;
 		DOM.instances.insertAdjacentHTML('beforeend', instanceHTML);
 	},
+
+	toggleAddInstanceForm() {
+		DOM.addInstanceForm.classList.toggle('hidden');
+	},
 };
 
 const Logic = {
 	// Logic methods
 };
+
+// Event Listeners
+DOM.addInstanceButton.addEventListener('click', UI.toggleAddInstanceForm);
